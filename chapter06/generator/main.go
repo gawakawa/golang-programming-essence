8efd719de19eaaa8a694a3bbd84e80da1c367d6b
gawakawa <mashurakiryu@icloud.com>
2025-08-18 20:53:25 +0900

    ✨ Add generator pattern implementation

---
+++ chapter06/generator/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func generator(msg string) <-chan string {
+	ch := make(chan string)
+	go func() {
+		for i := 0; ; i++ {
+			ch <- fmt.Sprintf("%s %d", msg, i)
+			time.Sleep(time.Second)
+		}
+	}()
+	return ch
+}
+
+func main() {
+	ch := generator("Hello")
+	for range 5 {
+		fmt.Println(<-ch)
+	}
+}
